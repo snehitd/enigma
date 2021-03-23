53cd7c0a1949f9ccf60ca2d54c90be40834ffa1c
snehitd <48319450+snehitd@users.noreply.github.com>
2021-03-23 19:49:32 +0530

    Create compare_strings.py

---
+++ compare_strings.py
@@ -0,0 +1,10 @@
+strA=input("Enter string A : ")
+#print(strA)
+strB=input("Enter string B : ")
+#print(strB)
+if strA == strB:
+    print("Strings are equal")
+elif strA > strB:
+    print(strA + " greater than " + strB)
+else:
+    print(strB + " greater than " + strA)
