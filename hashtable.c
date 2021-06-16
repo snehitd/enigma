139740f4fc5c33447b8df4324bb9f578e14dddd5
snehitd <48319450+snehitd@users.noreply.github.com>
2021-06-16 22:29:01 +0530

    Create hashtable.c

---
+++ hashtable.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#Hash Table
+key=>value lookup
+
+        hash function
+             |
+            \ /
+             | 
+string->hash code->index
+
+geeks for geeks
+https://www.youtube.com/watch?v=wWgIAphfn2U&list=PLqM7alHXFySGwXaessYMemAnITqlZdZVE 
+https://www.geeksforgeeks.org/hashing-set-1-introduction/
+
+Hackerrank
+https://www.youtube.com/watch?v=shs0KM3wKv8
+https://www.youtube.com/watch?v=TBfXjia6-UE
+
+Other
+https://www.journaldev.com/35238/hash-table-in-c-plus-plus
+https://www.log2base2.com/algorithms/searching/hashing-in-c-data-structure.html
+
+int main()
+{
+}
